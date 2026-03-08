ecb12999794f3cb8e6eb6e8a25f6127da74722d9
Saniya Meshram <saniya.meshram24@vit.edu>
2026-03-08 21:34:57 +0530

    updated code

--- frontend/src/api/orderApi.js
+++ frontend/src/api/orderApi.js
@@ -1,7 +1,22 @@
 // Assigned to: Saniya
-import api from './axiosConfig';
+import axios from "axios";
 
-// TODO: Implement order API calls
-// export const createOrder  = (data) => api.post('/orders', data);
-// export const getMyOrders  = ()     => api.get('/orders/myorders');
-// export const getOrderById = (id)   => api.get(`/orders/${id}`);
+export const createOrder = async (orderData) => {
+  const { data } = await axios.post("/api/orders", orderData);
+  return data;
+};
+
+export const getMyOrders = async () => {
+  const { data } = await axios.get("/api/orders/mine");
+  return data;
+};
+
+export const getOrderById = async (id) => {
+  const { data } = await axios.get(`/api/orders/${id}`);
+  return data;
+};
+
+export const updateOrderStatus = async (id, status) => {
+  const { data } = await axios.put(`/api/orders/${id}`, { status });
+  return data;
+};
